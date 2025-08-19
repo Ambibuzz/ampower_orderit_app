ea3ab33a192325ad40a0f941139152c255d8c59b
kaushalsatyamgupta <kaushalsatyamgupta@gmail.com>
2025-08-19 15:53:33 +0530

    initial commit

---
+++ ampower_orderit_app/ampower_orderit_community/doctype/orderit_community_config/test_orderit_community_config.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Ambibuzz and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestOrderITCommunityConfig(FrappeTestCase):
+	pass
